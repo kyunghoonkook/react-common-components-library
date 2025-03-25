89c3e3d434e9b8fc3f081fc898d776f7fabe710b
국경훈 <>
2025-03-25 14:52:01 +0900

    test

--- src/components-index/Popover.ts
+++ src/components-index/Popover.ts
@@ -2,5 +2,5 @@
  * Popover 컴포넌트 - 트리쉐이킹 최적화 진입점
  */
 import Popover from '../components/popover/Popover';
-export type { PopoverProps } from '../components/popover/Popover';
+export type { PopoverRootProps } from '../components/popover/Popover';
 export default Popover; 
--- src/components-index/Progress.ts
+++ src/components-index/Progress.ts
@@ -2,5 +2,5 @@
  * Progress 컴포넌트 - 트리쉐이킹 최적화 진입점
  */
 import Progress from '../components/progress/Progress';
-export type { ProgressProps } from '../components/progress/Progress';
+export type { ProgressRootProps } from '../components/progress/Progress';
 export default Progress; 
